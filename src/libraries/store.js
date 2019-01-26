e182bf77762ce5b8fc3b28c93586999f0f117466
Nicolas <nicolas@famille-lemire.com>
2019-01-26 10:52:51 +0100

    bootstrapped redux-Saga

--- src/libraries/store.js
+++ src/libraries/store.js
@@ -1,4 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 
-export default createStore(rootReducer);
+// const sagaMiddleware = createSagaMiddleware();
+// const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+// sagaMiddleware.runSaga(rootSaga);
+// export default store;
+
+function configureStore(initialState) {
+  // Note: passing middleware as the last argument to createStore requires redux@>=3.1.0
+  const sagaMiddleware = createSagaMiddleware();
+  return {
+    ...createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware)),
+    runSaga: sagaMiddleware.run
+  };
+}
+
+const store = configureStore();
+store.runSaga(rootSaga);
+
+export default store;
